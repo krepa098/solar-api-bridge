8b0dc547cbead01cfc984a98e2051060c4773abc
Paul <krepa098@gmail.com>
2025-11-02 11:43:13 +0100

    docker build --no-cache

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if docker build -t solar-api-bridge . ; then
+if docker build --no-cache -t solar-api-bridge . ; then
     IMAGE_FILE=solar-api-bridge.tgz
     docker save solar-api-bridge | gzip > $IMAGE_FILE
 
